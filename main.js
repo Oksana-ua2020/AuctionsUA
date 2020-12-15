53fd076eb338834dcc00ed9e0848daef5b1c158c
Oksana-ua2020 <mirisa5555@gmail.com>
2020-12-15 19:17:23 +0200

    HW 08/12

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 "use strict"
+function selectQuantity(select) {
+    console.log(select);
+    let Quantity = Number(select);
+    document.getElementById('pic-img').innerHTML = "";
+    for (let i = 1; i < Quantity+1; i++) {
+        let src = i + '.jpg';
+        document.getElementById('pic-img').innerHTML += `
+    <div class="pic">
+        <img src="https://picsum.photos/id/${i}/200/175" alt="product">
+    </div>`;
+    }
+}
+
 for (let i = 1; i < 11; i++) {
     let src = i + '.jpg';
     document.getElementById('pic-img').innerHTML += `
 <div class="pic">
-    <img src="img/${src}" alt="product">
-    <p">Standoff 2</p>
-    <div>
-        Game forever! I vsyo pofig!
-    </div>
+    <img src="https://picsum.photos/id/${i}/200/175" alt="product">
 </div>`;
 }
