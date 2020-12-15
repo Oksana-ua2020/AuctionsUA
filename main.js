67bfa69144441b78aed7a29f0e16357482fd5873
Oksana Ratova <oksanaratova@MacBook-Pro-Oksana.local>
2020-12-15 22:08:32 +0200

    ClW

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 "use strict"
+let object__product = {
+    image: "https://picsum.photos/200/175",
+    number:  1,
+    price: 128,
+}
 function selectQuantity(select) {
     console.log(select);
     let Quantity = Number(select);
-    document.getElementById('pic-img').innerHTML = "";
+    let result = "";
+
     for (let i = 1; i < Quantity+1; i++) {
-        let src = i + '.jpg';
-        document.getElementById('pic-img').innerHTML += `
+        result += `
     <div class="pic">
-        <img src="https://picsum.photos/id/${i}/200/175" alt="product">
+        <img src = "${object__product.image}" alt="product">
+        <p>Price na fig ${Math.round()}</p>
     </div>`;
     }
+    document.getElementById('pic-img').innerHTML = result;
 }
 
-for (let i = 1; i < 11; i++) {
-    let src = i + '.jpg';
-    document.getElementById('pic-img').innerHTML += `
-<div class="pic">
-    <img src="https://picsum.photos/id/${i}/200/175" alt="product">
-</div>`;
-}
+selectQuantity(10);
+
